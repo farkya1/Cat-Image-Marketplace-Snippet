5f9ad41240b8f4fadab3c99f80553c0980657f94
farkya1 <94140990+farkya1@users.noreply.github.com>
2024-07-28 19:27:46 -0400

    Delete flask_app/database/create_tables/feedback.sql

--- flask_app/database/create_tables/feedback.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS `feedback` (
-`feedback_id`        int(11)       NOT NULL AUTO_INCREMENT 	COMMENT 'The feedback id',
-`name`           varchar(100)  NOT NULL                	COMMENT 'The name of the commenter',
-`email`          varchar(100)  NOT NULL                	COMMENT 'The email of the commenter',
-`feedback`        varchar(500)  NOT NULL                	COMMENT 'The feedback',
-PRIMARY KEY  (`feedback_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT="Comments left on the website";
